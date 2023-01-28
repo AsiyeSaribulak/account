bc3d1dcc9b1fb669be6df89d8330485a3c82bcec
Asiye Sarıbulak <5364542@ogrenci.bilecik.edu.tr>
2023-01-29 00:10:27 +0300

    getCustomerById method added

--- src/main/java/com/asiyesaribulak/account/controller/CustomerController.java
+++ src/main/java/com/asiyesaribulak/account/controller/CustomerController.java
@@ -1,8 +1,15 @@
 package com.asiyesaribulak.account.controller;
 
+import com.asiyesaribulak.account.dto.CustomerDto;
 import com.asiyesaribulak.account.service.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/V1/customer")
@@ -12,4 +19,12 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
+@GetMapping("/{customerId}getCustomerById")
+    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable @Valid String customerId){
+        return ResponseEntity.ok(customerService.getCustomerById(customerId));
+}
+@GetMapping("/getAllCustomers")
+    public ResponseEntity<List<CustomerDto>> getAllCustomer(){
+        return ResponseEntity.ok(customerService.getAllCustomer());
+}
 }
